699aa27a0cc83ad3131eaad811c84084c3398ddc
ktrzeciaknubisa <ktrzeciak@nubisa.com>
2015-02-18 20:26:19 +0100

    cleanup

--- samples/jx_packaging/basics/test.js
+++
@@ -1,8 +0,0 @@
-/*JX package sample - check README.md for instructions*/
-
-var t = require("./sub/sample1.jx")
-
-console.log("\n");
-
-console.log("Source hiddenFunction :", t.hiddenFunction.toString());
-console.log("Source normalFunction :", t.normalFunction.toString());
