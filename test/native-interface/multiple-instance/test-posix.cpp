1840203daf2b0fc5d5f858c8cc3e0f64de48b520
Oguz Bastemur <obastemur@gmail.com>
2015-03-12 22:56:48 +0100

    add comments

--- test/native-interface/multiple-instance/test-posix.cpp
+++ test/native-interface/multiple-instance/test-posix.cpp
@@ -5,6 +5,7 @@ void callback(JXResult *results, int argc) {
   // do nothing
 }
 
+// native JavaScript method we'll be calling from JS land
 void sampleMethod(JXResult *results, int argc) {
   std::stringstream ss_result;
   for (int i = 0; i < argc; i++) {
@@ -35,10 +36,18 @@ const char *eval_str =
     "webview.call([\"concat\",\"A\",\"B\",{\"jxcore_webview_callbackId\":1}]);";
 
 void *create_jxcore_instance(void *_) {
+  // create a new JXcore instance
   JX_InitializeNewEngine();
 
+  // define entry file
   JX_DefineMainFile(contents);
+  
+  // define native -named- method
+  // we will be reaching to this method from the javascript side like this;
+  // process.natives.sampleMethod( ... )
   JX_DefineExtension("sampleMethod", sampleMethod);
+  
+  // start the engine (executes entry file)
   JX_StartEngine();
 
   // loop for possible IO
@@ -46,19 +55,23 @@ void *create_jxcore_instance(void *_) {
   while (JX_LoopOnce() != 0) usleep(1);
 
   JXResult result;
+  // evaluate a piece of JavaScript code
   JX_Evaluate(eval_str, "myscript", &result);
 
+  // see if the result is a JavaScript object
   if(!JX_ResultIsJSON(&result)) {
 	flush_console("RETURN TYPE WAS: %d\n", result.type_);
   }
   assert(JX_ResultIsJSON(&result) && "expected result here is a JSON (JS array)");
 
+  // free the memory
   JX_FreeResultData(&result);
 
   // loop for possible IO
   // or JX_Loop() without usleep / while
   while (JX_LoopOnce() != 0) usleep(1);
 
+  // destroy the instance
   JX_StopEngine();
   return 0;
 }
@@ -66,15 +79,32 @@ void *create_jxcore_instance(void *_) {
 #define NUM_THREADS 20
 
 int main(int argc, char **args) {
+  // Call JX_Initialize only once per app	
   JX_Initialize(args[0], callback);
+  
+  // Creates a new engine for the current thread
+  // It's our first engine instance hence it will be the
+  // parent engine for all the other engine instances.
+  // If you need to destroy this engine instance, you should
+  // destroy everything else first. For the sake of this sample
+  // we have our first instance sitting on the main thread
+  // and it will be destroyed when the app exists.
   JX_InitializeNewEngine();
 
   pthread_t thread[NUM_THREADS];
 
+  // define the entry file contents
   JX_DefineMainFile(contents);
+  
+  // define native -named- method
+  // we will be reaching to this method from the javascript side like this;
+  // process.natives.sampleMethod( ... )
   JX_DefineExtension("sampleMethod", sampleMethod);
+  
+  // start the engine (executes entry file)
   JX_StartEngine();
 
+  // create and run other instances 
   for (int i = 0; i < NUM_THREADS; i++) {
     assert(pthread_create(&thread[i], NULL, create_jxcore_instance, 0) == 0);
   }
@@ -84,8 +114,10 @@ int main(int argc, char **args) {
   while (JX_LoopOnce() != 0) usleep(1);
 
   JXResult result;
+  // evaluate a piece of JavaScript code
   JX_Evaluate(eval_str, "myscript", &result);
 
+  // free the memory
   JX_FreeResultData(&result);
 
   // loop for possible IO
@@ -96,5 +128,6 @@ int main(int argc, char **args) {
     pthread_join(thread[i], NULL);
   }
 
+  // finally destroy the first instance
   JX_StopEngine();
 }
