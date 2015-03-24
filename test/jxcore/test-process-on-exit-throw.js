ab545711c1c755beb5c4331649f165f4f7b3a5c3
ktrzeciaknubisa <ktrzeciak@nubisa.com>
2015-03-24 17:12:50 +0100

    using file instead of stdout

--- test/jxcore/test-process-on-exit-throw.js
+++ test/jxcore/test-process-on-exit-throw.js
@@ -5,16 +5,18 @@
  It is performed inside child process to be able to catch the output.
  */
 
+var fs = require("fs");
+var path = require("path");
 
 var isChild = process.argv[process.argv.length - 1] == "child";
 var str_exitting = "Exiting with code";
 var str_throw = "Error intended";
-
+var outFileName = process.cwd() + path.sep + "test-process-on-exit-throw_out.txt";
 
 if (isChild) {
   // this is the proper test case
   process.on('exit', function (code) {
-    console.log(str_exitting, code);
+      fs.writeFileSync(outFileName, str_exitting);
   });
 
   throw str_throw;// new Error(str_throw);
@@ -31,11 +33,16 @@ if (!isChild) {
   var fname = process.IsEmbedded ? process.execPath : __filename.replace(".js.jx", ".jx");
   var child = cp.exec('"' + process.execPath + '" "' + fname + '" child', null, function (error, stdout, stderr) {
 
-    var str = "" + stdout + stderr;
-    //console.log("error", error, "\n", "stdout " + stdout, "\n", "stderr " + stderr);
+    var exists = fs.existsSync(outFileName);
+    assert.ok(exists, "Could not find output written to file " + outFileName);
 
+    // checking results of mt
+    var ret = fs.readFileSync(outFileName).toString();
+    fs.unlinkSync(outFileName);
+
+    var str = "" + stdout + stderr;
     assert.ok(str.indexOf(str_throw) !== -1, "Cannot determine if child have thrown an error.");
-    assert.ok(str.indexOf(str_exitting) !== -1, "Child did not call on('exit') after throw.");
+    assert.ok(ret.indexOf(str_exitting) !== -1, "Child did not call on('exit') after throw.");
 
     finished = true;
   });
