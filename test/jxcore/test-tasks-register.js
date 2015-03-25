d00944ca68a842adc4c44b3543a9fd1cc7410259
ktrzeciaknubisa <ktrzeciak@nubisa.com>
2015-03-25 17:12:50 +0100

    tasks.register

---
+++ test/jxcore/test-tasks-register.js
@@ -0,0 +1,33 @@
+// Copyright & License details are available under JXCORE_LICENSE file
+
+
+var assert = require('assert');
+
+var methods = function () {
+  global.cwdToLower = function () {
+    return process.cwd().toLowerCase();
+  };
+
+  global.myVar = "something";
+};
+
+var task = function () {
+
+  var ret = {};
+  try {
+    ret.cwdToLower = cwdToLower();
+    ret.myVar = myVar;
+  } catch (ex) {
+  }
+
+  return ret;
+};
+
+jxcore.tasks.register(methods);
+
+jxcore.tasks.addTask(task, null, function (ret) {
+  methods();
+  var main_ret = task();
+  for (var o in main_ret)
+    assert.strictEqual(main_ret[o], ret[o], "Values are not equal: main_ret[" + o + "] !== ret[" + o + "]");
+});
