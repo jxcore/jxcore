022214d6f6fcaac0294d4c12af7f6c3e11d1d64a
obastemur <obastemur@gmail.com>
2015-04-03 02:34:48 +0200

    update multi-tasking test - thread recovery

--- test/simple/test-api-tasks.js
+++ test/simple/test-api-tasks.js
@@ -1,13 +1,22 @@
+// Copyright & License details are available under JXCORE_LICENSE file
+
+
 var reset = true;
 
-jxcore.tasks.setThreadCount(4);
+jxcore.tasks.setThreadCount(2);
 
+  var counter = 0;
 function test() {
   reset = false;
-  var counter = 0;
-  for (var i = 0; i < 20; i++) {
+  for (var i = 0; i < 10; i++) {
     jxcore.tasks.addTask(function (a) {
-      require('./test-buffer-concat');
+      require('fs').ReadStream(__filename)
+				.on('data', function (d) {})
+				.on('end', function () {
+					console.log("finished");
+					process.release();
+			});
+
       var str = "";
       for(var o in process.env) {
         str += o + " : " + process.env[o];
@@ -20,10 +29,15 @@ function test() {
 
       return a + buffer[a];
     }, i, function (retval) {
-      counter++;
-      if (counter == 20) {
+        counter++;
+      if (counter == 10) {
+        counter = 0;
         jxcore.tasks.unloadThreads();
-        reset = true;
+        
+        // we need a callback for threads unloaded!
+        setTimeout(function(){
+          reset = true;
+        }, 500);
         console.log("RESET", Date.now());
       }
     });
@@ -34,7 +48,7 @@ var ll = 0;
 var inter = setInterval(function(){
   if (reset) {
     ll ++;
-    if(ll>20) {
+    if(ll>10) {
       clearInterval(inter);
       process.exit(0);
     } else {
