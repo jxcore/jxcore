ed70e039c084a3402220d96c8d3d0f98e2772962
ktrzeciaknubisa <ktrzeciak@nubisa.com>
2015-04-09 21:36:47 +0200

    extended slim test

---
+++ test/jxcore/assets/src/a/b/src/src2.js
@@ -0,0 +1,1 @@
+exports.value = "src2.js";
---
+++ test/jxcore/assets/test-packaging-slim.js
@@ -0,0 +1,42 @@
+// Copyright & License details are available under JXCORE_LICENSE file
+
+
+var fs = require("fs");
+var path = require("path");
+
+var json_file = path.join(__dirname, "config.json");
+if (!fs.existsSync(json_file)) {
+  console.error("The config.json file does not exists:", json_file);
+  process.exit(1);
+}
+
+var json_str = fs.readFileSync(json_file).toString();
+var json = {};
+try {
+  json = JSON.parse(json_str);
+} catch (ex) {
+  console.error("Cannot parse json string:");
+  console.error(json_str);
+  process.exit(1);
+}
+
+
+for (var o in json.should_be_readable) {
+  var relative_path = json.should_be_readable[o];
+  try {
+    fs.readFileSync(relative_path).toString();
+  } catch(ex) {
+    console.error("Cannot read file from package:\n\t" + relative_path);
+  }
+}
+
+
+for (var o in json.should_not_be_readable) {
+  var relative_path = json.should_be_readable[o];
+  try {
+    fs.readFileSync(relative_path).toString();
+    console.error("File should not be readable from package:" + relative_path);
+  } catch(ex) {
+    // ok
+  }
+}
