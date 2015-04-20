bcbed47f26675a62a15d597eea63d79e6992e6ca
obastemur <obastemur@gmail.com>
2015-04-20 03:51:04 +0200

    extern createThread

--- src/jx/job.h
+++ src/jx/job.h
@@ -3,13 +3,29 @@
 #ifndef SRC_JX_JOB_H_
 #define SRC_JX_JOB_H_
 
+#ifndef JXCORE_EXTERN
+#ifdef _WIN32
+#define JXCORE_EXTERN(x) __declspec(dllexport) x
+#else
+#define JXCORE_EXTERN(x) x
+#endif
+#endif
+
 namespace jxcore {
 void SendMessage(const int threadId, const char *msg_data, const int length,
                  bool same_thread);
 
 int CreateInstances(const int count);
 
-int CreateThread(void (*entry)(void *arg), void *param);
+#ifdef JS_ENGINE_V8
+// TODO(obastemur) implement libuv thread join
+JXCORE_EXTERN(int)
+#elif defined(JS_ENGINE_MOZJS)
+JXCORE_EXTERN(bool) JoinThread(void *pth);
+
+JXCORE_EXTERN(void *)
+#endif
+    CreateThread(void (*entry)(void *arg), void *param);
 }
 
 #endif  // SRC_JX_JOB_H_
