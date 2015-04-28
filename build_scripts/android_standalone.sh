f2d08e208dc529673ad578736d6b2f0c97e02519
Oguz Bastemur <obastemur@gmail.com>
2015-04-28 11:56:57 +0200

    android standalone compiler script

--- build_scripts/android_standalone.sh
+++ build_scripts/android_standalone.sh
@@ -1,4 +1,3 @@
-
 #!/bin/bash
 
 NORMAL_COLOR='\033[0m'
@@ -31,32 +30,80 @@ ERROR_ABORT_MOVE() {
   fi
 }
 
-if [ $# -eq 0 ]
+SHOW_USAGE() {
+  LOG $GREEN_COLOR "usage: android_compile_v8 <ndk_path> <arch>"
+  LOG $GREEN_COLOR "arch: intel arm mips"
+  exit
+}
+
+if [ $# -lt 2 ]
 then
   LOG $RED_COLOR "no argument provided."
-  LOG $GREEN_COLOR "usage: android_compile_v8 <ndk_path>\n"
-  exit
+  SHOW_USAGE
 fi
 
 export ANDROID_NDK=$1
     
 MAKE_INSTALL() {
-  PREFIX_DIR="out_android_$1"
-  ./configure --prefix=$PREFIX_DIR --dest-os=android --dest-cpu=$1 --without-snapshot
+  TARGET_DIR="out_android/sa_$1"
+
+  mkdir -p $TARGET_DIR  
+  mv $TARGET_DIR out
+  ./configure --prefix=$TARGET_DIR/__/ --dest-os=android --dest-cpu=$1 --without-snapshot
+  ERROR_ABORT_MOVE "mv out $TARGET_DIR" $1
+  
   make
+  ERROR_ABORT_MOVE "mv out $TARGET_DIR" $1
+  
+  mv out $TARGET_DIR
 }
 
+rm -rf out
+
 OLD_PATH=$PATH
+CPU_TYPE=0
 
-export TOOLCHAIN=$PWD/android-toolchain-intel
-export PATH=$TOOLCHAIN/bin:$OLD_PATH
-export AR=i686-linux-android-ar
-export CC=i686-linux-android-gcc
-export CXX=i686-linux-android-g++
-export LINK=i686-linux-android-g++
+if [ $2 == 'intel' ]
+then
+  export TOOLCHAIN=$PWD/android-toolchain-intel
+  export PATH=$TOOLCHAIN/bin:$OLD_PATH
+  export AR=i686-linux-android-ar
+  export CC=i686-linux-android-gcc
+  export CXX=i686-linux-android-g++
+  export LINK=i686-linux-android-g++
+  CPU_TYPE='ia32'
+fi
 
-LOG $GREEN_COLOR "Compiling Android INTEL32\n"
-MAKE_INSTALL ia32
+if [ $2 == 'arm' ]
+then
+  export TOOLCHAIN=$PWD/android-toolchain-arm
+  export PATH=$TOOLCHAIN/bin:$OLD_PATH
+  export AR=arm-linux-androideabi-ar
+  export CC=arm-linux-androideabi-gcc
+  export CXX=arm-linux-androideabi-g++
+  export LINK=arm-linux-androideabi-g++
+  CPU_TYPE='arm'
+fi
+
+if [ $2 == 'mips' ]
+then
+  export TOOLCHAIN=$PWD/android-toolchain-mipsel
+  export PATH=$TOOLCHAIN/bin:$OLD_PATH
+  export AR=mipsel-linux-android-ar
+  export CC=mipsel-linux-android-gcc
+  export CXX=mipsel-linux-android-g++
+  export LINK=mipsel-linux-android-g++
+  CPU_TYPE='mipsel'
+fi
+
+if [ $CPU_TYPE == 0 ]
+then
+  LOG $RED_COLOR "Unknown architecture."
+  SHOW_USAGE
+fi
+
+LOG $GREEN_COLOR "Compiling JXcore standalone for Android-$2\n"
+MAKE_INSTALL $CPU_TYPE
 
 
-LOG $GREEN_COLOR "JXcore Android binaries are ready\n"
+LOG $GREEN_COLOR "jx binary is ready under out_android/sa_$CPU_TYPE/Release/\n"
