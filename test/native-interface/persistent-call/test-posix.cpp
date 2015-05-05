541f61407dc00fbdc7c45ad7dfd6fc6d47c452a6
obastemur <obastemur@gmail.com>
2015-05-05 21:34:33 +0200

    add useful comments on persistent call test

--- test/native-interface/persistent-call/test-posix.cpp
+++ test/native-interface/persistent-call/test-posix.cpp
@@ -11,6 +11,7 @@ const char compare_base[10] = {'1', '4', 't', 't', '{',
 JXValue *fnc;
 JXValue *param1, *param2;
 
+// native sampleMethod
 void sampleMethod(JXValue *results, int argc) {
   for (int i = 0; i < argc; i++) {
     std::string str;
@@ -23,12 +24,17 @@ void sampleMethod(JXValue *results, int argc) {
 
   JXValue out;
 
+  // fnc represents the JS side function
   fnc = &results[9];
   param1 = results + 3;
   param2 = results + 4;
 
+  // call JS side fnc with 2 parameters and get
+  // the result to 'out'
   JX_CallFunction(fnc, (results + 3), 2, &out);
 
+  // make fnc persistent so we can call it later again
+  // see 'int main' for second call
   JX_MakePersistent(fnc);
   JX_MakePersistent(param1);
   JX_MakePersistent(param2);
@@ -40,8 +46,10 @@ void sampleMethod(JXValue *results, int argc) {
 }
 
 const char *contents =
+	// define a JS side function fnc
     "function fnc(x, y){return (x + JSON.stringify(y));}\n"
     "var bf = new Buffer(5);bf.fill(65);\n"
+	// deliver this function 'last' parameter to the native sampleMethod above
     "process.natives.sampleMethod(1, 4.5, true, 'test', {a:3}, bf, undefined, "
     "null, new Error('error!'), fnc);";
 
@@ -57,8 +65,13 @@ int main(int argc, char **args) {
 
   JXValue *params[2] = {param1, param2};
   JXValue out;
+
+  // call fnc -> JS side function
+  // we had made this variable persistent inside sampleMethod
   JX_CallFunction(fnc, *params, 2, &out);
 
+  // we need to clear persistent and then free whenever we
+  // are done with persistent values to not to leak
   JX_ClearPersistent(fnc);
   JX_Free(fnc);
   JX_ClearPersistent(param1);
