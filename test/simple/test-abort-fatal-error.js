5da939a2928787fe3176e7282848cd4ab4049a2c
ktrzeciaknubisa <ktrzeciak@nubisa.com>
2015-05-19 17:29:39 +0200

    android fixtures

--- test/simple/test-abort-fatal-error.js
+++ test/simple/test-abort-fatal-error.js
@@ -19,5 +19,10 @@ cmdline += ' -e "setInterval(function() { new Buffer(1024); }, 1);"';
 
 exec(cmdline, function(err, stdout, stderr) {
   assert(err);
-  assert(stderr.toString().match(/abort/i));
+
+  // works for all platforms, also for android
+  assert(stderr.toString().match(/process out of memory/i));
+
+  if (process.platform !== "android")
+    assert(stderr.toString().match(/abort/i));
 });
--- test/simple/test-child-process-set-blocking.js
+++ test/simple/test-child-process-set-blocking.js
@@ -8,9 +8,12 @@ var ch = require('child_process');
 var SIZE = 100000;
 var childGone = false;
 
-var cp = ch.spawn('python', ['-c', 'print ' + SIZE + ' * "C"'], {
-  customFds: [0, 1, 2]
-});
+if (process.platform === "android") {
+  var _eval = 'var size = ' + SIZE + '; while(size--) { process.stdout.write("C"); }';
+  var cp = ch.spawn(process.execPath, ['-e', _eval], { customFds: [0, 1, 2] });
+} else {
+  var cp = ch.spawn('python', ['-c', 'print ' + SIZE + ' * "C"'], { customFds: [0, 1, 2] });
+}
 
 cp.on('exit', function(code) {
   childGone = true;
