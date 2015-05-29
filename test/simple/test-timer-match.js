3cbea50e28c6594d968003a9e0ce0fe679de8f5e
Oguz Bastemur <obastemur@gmail.com>
2015-05-29 13:28:50 +0200

    fix test case (there is no Exception type)

--- test/simple/test-timer-match.js
+++ test/simple/test-timer-match.js
@@ -3,20 +3,20 @@
 // timer uses Timer.now while a JS application might be depending Date.now
 // This test case checks whether setTimeout etc. event fires sooner than expected
 
-var last_time = Date.now() - 1;
+var last_time = Date.now();
 var counter = 0;
 
 var test = function() {
   var time = Date.now();
 
   if (time == last_time) {
-    throw new Exception("Fail!");
+    throw new Error("Fail!");
   }
 
   if (++counter % 500 == 0) {
     console.log("]]", counter);
 
-    if (counter >= 2000) {
+    if (counter >= 2500) {
       process.exit(0);
     }
   }
