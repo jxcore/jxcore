da509a90e9d92adbec6725b1eeddc3f57019fd9d
ktrzeciaknubisa <ktrzeciak@nubisa.com>
2015-08-19 15:51:00 +0200

    test fix: ignore system output

--- test/jxcore/assets/test-packaging-slim.js
+++ test/jxcore/assets/test-packaging-slim.js
@@ -4,9 +4,24 @@
 var fs = require("fs");
 var path = require("path");
 
+var testErrorWritten = false;
+
+var logError = function() {
+  if (!testErrorWritten){
+    console.error("TEST ERROR")
+    testErrorWritten = true;
+  }
+  console.error.apply(null, arguments);
+};
+
+process.on("exit", function(code) {
+  if (code)
+    logError("Exiting with code", code);
+});
+
 var json_file = path.join(__dirname, "config.json");
 if (!fs.existsSync(json_file)) {
-  console.error("The config.json file does not exists:", json_file);
+  logError("The config.json file does not exists:", json_file);
   process.exit(1);
 }
 
@@ -15,8 +30,8 @@ var json = {};
 try {
   json = JSON.parse(json_str);
 } catch (ex) {
-  console.error("Cannot parse json string:");
-  console.error(json_str);
+  logError("Cannot parse json string:");
+  logError(json_str);
   process.exit(1);
 }
 
@@ -29,7 +44,7 @@ for (var o in json.should_be_readable) {
   try {
     fs.readFileSync(relative_path).toString();
   } catch(ex) {
-    console.error("Cannot read file from package:\n\t" + relative_path);
+    logError("Cannot read file from package:\n\t" + relative_path);
   }
 }
 
@@ -41,7 +56,7 @@ for (var o in json.should_not_be_readable) {
   var relative_path = json.should_not_be_readable[o];
   try {
     fs.readFileSync(relative_path).toString();
-    console.error("File should not be readable from package:" + relative_path);
+    logError("File should not be readable from package:" + relative_path);
   } catch(ex) {
     // ok
   }
