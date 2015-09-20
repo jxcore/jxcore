20ca629ea9385c41c8572307403e99ee78d5fea0
ktrzeciaknubisa <ktrzeciak@nubisa.com>
2015-09-20 12:55:33 +0200

    package execution test

---
+++ test/jxcore/assets/test-packaging-extract-exec/async.js
@@ -0,0 +1,5 @@
+// Copyright & License details are available under JXCORE_LICENSE file
+
+setTimeout(function() {
+  console.log(process.argv[2]);
+}, 1000);
---
+++ test/jxcore/assets/test-packaging-extract-exec/sync.js
@@ -0,0 +1,3 @@
+// Copyright & License details are available under JXCORE_LICENSE file
+
+console.log(process.argv[2]);
