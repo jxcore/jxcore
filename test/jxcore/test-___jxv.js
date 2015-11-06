cc387174641cb784aa993ef42b2117a622e70e4d
ktrzeciaknubisa <ktrzeciak@nubisa.com>
2015-11-06 17:23:33 +0100

    new version check

--- test/jxcore/test-___jxv.js
+++ test/jxcore/test-___jxv.js
@@ -5,11 +5,33 @@ var fs = require('fs');
 var expectedVersion;
 
 var vers = fs.readFileSync('src/node_version.h') + "";
-var index = vers.indexOf('0.3.');
-var index2 = vers.indexOf('"', index);
-var str = vers.substr(index+4, index2-(index+4));
 
-expectedVersion = "v 0.3." + str;
+
+var getNumber = function(reg) {
+  var arr =  reg.exec(vers);
+  if (!arr[1])
+    return null;
+
+  var ret = parseInt(arr[1]);
+  if (isNaN(ret))
+    return null;
+
+  return ret;
+};
+
+var major =  getNumber(/#define JXCORE_MAJOR_VERSION (\d)/);
+var minor =  getNumber(/#define JXCORE_MINOR_VERSION (\d)/);
+var patch =  getNumber(/#define JXCORE_PATCH_VERSION (\d)/);
+
+if (major !== null && minor !== null && patch !== null) {
+  expectedVersion = "v 0." + major + '.' + minor + '.' + patch;
+} else {
+  // older versions
+  var index = vers.indexOf('0.3.');
+  var index2 = vers.indexOf('"', index);
+  var str = vers.substr(index+4, index2-(index+4));
+  expectedVersion = "v 0.3." + str;
+}
 
 var color = jxcore.utils.console.setColor;
 var str_insteadof = " instead of " + color(expectedVersion, "green");
