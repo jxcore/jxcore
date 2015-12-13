jxcore/jxcore

commit 9932cdac376fb6afc8e68fc2d5eb94ab301a4697

subthreads waiting for package to load. Fixes #709

lib/module.js
@@ -76,6 +76,11 @@ function statPath(path) {
   return false;
 }
 
+function isThreadLoadingRoots() {
+  return (!process.subThread && process.threadId == -1) ||
+    (process._MTED && process.threadId == 0);
+}
+
 var __existsSync = function(_path) {  // checks the file system
 
   try {
@@ -848,6 +853,27 @@ Module.runMain = function() {
 
 var _loadSub = function() {
   process.reloadModules = function() {
+
+    if (!isThreadLoadingRoots()) {
+      // since process.reloadModules is called only once per thread,
+      // we should wait a little for NativeModule.Roots
+      var _start = Date.now();
+      var _warningDisplayed = false;
+      while (!$uw.existsSource('NativeModule.Roots')) {
+        sleep(2);
+        var dx = Date.now() - _start;
+        // practically we should wait as long as it takes...
+        // however let's set up a limit 20 secs (for larger packages)
+        if (dx > 5000 && !_warningDisplayed) {
+          jxcore.utils.console.warn('Thread no', process.threadId, 'is still waiting for package to be loaded.');
+          _warningDisplayed = true;
+        } else if (dx > 20000) {
+          jxcore.utils.console.warn('Thread no', process.threadId, 'couldn\'t wait any longer.');
+          break;
+        }
+      }
+    }
+
     if ($uw.existsSource('NativeModule.Roots')) {
       NativeModule.RootsLength = 1;
       NativeModule.Roots = JSON.parse($uw.readSource('NativeModule.Roots'));
@@ -1476,8 +1502,7 @@ var readX = function(m, f, obj, pa, eo) {
     }
   }
 
-  if ((!process.subThread && process.threadId == -1) ||
-      (process._MTED && process.threadId == 0)) {
+  if (isThreadLoadingRoots()) {
     $uw.setSource('NativeModule.Roots', JSON.stringify(NativeModule.Roots));
   }
 

commit fbfd084818884a5389dd93f185b3a44026da3452

Fix bug introduced with 9932cdac376fb6afc8e68fc2d5eb94ab301a4697

lib/jx/_jx_subs.js
@@ -49,11 +49,6 @@ if (!resolved_set) {
   require.main.filename = resolved;
 }
 
-if (process.reloadModules) {
-  process.reloadModules();
-  delete(process.reloadModules);
-}
-
 if (require.main.fileSource)
   require.main.fileSource = null;
 

lib/module.js
@@ -833,7 +833,6 @@ var jxt = process.binding('jxutils_wrap');
 
 Module.runMain = function() {
   if (process.subThread) {
-    _loadSub();
     Module._load('_jx_subs', null, true);
   }
   else if (process._Monitor) {
@@ -852,42 +851,37 @@ Module.runMain = function() {
 };
 
 var _loadSub = function() {
-  process.reloadModules = function() {
+  if (process.threadId == -1) return;
+  
+  if (!isThreadLoadingRoots()) {
+    // since process.reloadModules is called only once per thread,
+    // we should wait a little for NativeModule.Roots
+    var _start = Date.now();
+    while (!$uw.existsSource('NativeModule.Roots')) {
+      sleep(2);
+      var dx = Date.now() - _start;
+      // practically we should wait as long as it takes...
+      // however let's set up a limit 5 secs (for larger packages)
+      if (dx > 5000) {
+        jxcore.utils.console.warn('Thread no', process.threadId, ' will be reloading JX package into memory.\nYou might create this instance after the JX package is loaded.');
+        break;
+      }
+    }
+  }
 
-    if (!isThreadLoadingRoots()) {
-      // since process.reloadModules is called only once per thread,
-      // we should wait a little for NativeModule.Roots
-      var _start = Date.now();
-      var _warningDisplayed = false;
-      while (!$uw.existsSource('NativeModule.Roots')) {
-        sleep(2);
-        var dx = Date.now() - _start;
-        // practically we should wait as long as it takes...
-        // however let's set up a limit 20 secs (for larger packages)
-        if (dx > 5000 && !_warningDisplayed) {
-          jxcore.utils.console.warn('Thread no', process.threadId, 'is still waiting for package to be loaded.');
-          _warningDisplayed = true;
-        } else if (dx > 20000) {
-          jxcore.utils.console.warn('Thread no', process.threadId, 'couldn\'t wait any longer.');
-          break;
+  if ($uw.existsSource('NativeModule.Roots')) {
+    NativeModule.RootsLength = 1;
+    NativeModule.Roots = JSON.parse($uw.readSource('NativeModule.Roots'));
+    for (var o in NativeModule.Roots) {
+      if (!NativeModule.Roots.hasOwnProperty(o)) continue;
+      var bns = NativeModule.Roots[o];
+      for (var b in bns) {
+        if (bns.hasOwnProperty(b) && bns[b] && bns[b].size) {
+          bns[b] = new fs.JXStats(null, null, bns[b]);
         }
       }
     }
-
-    if ($uw.existsSource('NativeModule.Roots')) {
-      NativeModule.RootsLength = 1;
-      NativeModule.Roots = JSON.parse($uw.readSource('NativeModule.Roots'));
-      for (var o in NativeModule.Roots) {
-        if (!NativeModule.Roots.hasOwnProperty(o)) continue;
-        var bns = NativeModule.Roots[o];
-        for (var b in bns) {
-          if (bns.hasOwnProperty(b) && bns[b] && bns[b].size) {
-            bns[b] = new fs.JXStats(null, null, bns[b]);
-          }
-        }
-      }
-    }
-  };
+  }
 };
 
 getMemContent = function(location) {
@@ -1363,6 +1357,10 @@ var readX = function(m, f, obj, pa, eo) {
       runActions(eo['post-actions'], 'extract-post-actions',
           obj.project.native, extract_verbose);
   }
+  
+  if (isThreadLoadingRoots()) {
+    $uw.setSource('NativeModule.Roots', JSON.stringify(NativeModule.Roots));
+  }
 
   if (entry_file) {
     var exito = false;
@@ -1500,10 +1498,6 @@ var readX = function(m, f, obj, pa, eo) {
       }
       return false;
     }
-  }
-
-  if (isThreadLoadingRoots()) {
-    $uw.setSource('NativeModule.Roots', JSON.stringify(NativeModule.Roots));
   }
 
   var was_embedded = process._EmbeddedSource;
@@ -1910,6 +1904,8 @@ Module._extensions['.jx'] = function(m, f, pa) {
     Module._extensions['.jsonx'](m, f);
     return;
   }
+  
+  _loadSub();
 
   if ($uw.existsSource('@' + f)) {
     m._compile('@' + f, f, true, true);