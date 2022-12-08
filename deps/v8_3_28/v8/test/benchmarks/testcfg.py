91676bbcd7e3cb2d2f66997c9cfa0ea2a8203f7e
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-12-08 11:43:26 +0000

    Adding tarfile member sanitization to extractall()

--- deps/v8_3_28/v8/test/benchmarks/testcfg.py
+++ deps/v8_3_28/v8/test/benchmarks/testcfg.py
@@ -148,7 +148,26 @@ class BenchmarksTestSuite(testsuite.TestSuite):
     archive_file = "downloaded_%s_%s.tar.gz" % (target_dir, revision)
     if os.path.exists(archive_file):
       with tarfile.open(archive_file, "r:gz") as tar:
-        tar.extractall()
+        def is_within_directory(directory, target):
+            
+            abs_directory = os.path.abspath(directory)
+            abs_target = os.path.abspath(target)
+        
+            prefix = os.path.commonprefix([abs_directory, abs_target])
+            
+            return prefix == abs_directory
+        
+        def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+        
+            for member in tar.getmembers():
+                member_path = os.path.join(path, member.name)
+                if not is_within_directory(path, member_path):
+                    raise Exception("Attempted Path Traversal in Tar File")
+        
+            tar.extractall(path, members, numeric_owner=numeric_owner) 
+            
+        
+        safe_extract(tar)
       with open(revision_file, "w") as f:
         f.write(revision)
       return
--- deps/v8_3_28/v8/test/mozilla/testcfg.py
+++ deps/v8_3_28/v8/test/mozilla/testcfg.py
@@ -140,7 +140,26 @@ class MozillaTestSuite(testsuite.TestSuite):
     archive_file = "downloaded_%s.tar.gz" % MOZILLA_VERSION
     if os.path.exists(archive_file):
       with tarfile.open(archive_file, "r:gz") as tar:
-        tar.extractall()
+        def is_within_directory(directory, target):
+            
+            abs_directory = os.path.abspath(directory)
+            abs_target = os.path.abspath(target)
+        
+            prefix = os.path.commonprefix([abs_directory, abs_target])
+            
+            return prefix == abs_directory
+        
+        def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+        
+            for member in tar.getmembers():
+                member_path = os.path.join(path, member.name)
+                if not is_within_directory(path, member_path):
+                    raise Exception("Attempted Path Traversal in Tar File")
+        
+            tar.extractall(path, members, numeric_owner=numeric_owner) 
+            
+        
+        safe_extract(tar)
       with open(versionfile, "w") as f:
         f.write(MOZILLA_VERSION)
       os.chdir(old_cwd)
